8b0f71326e80a64065426c3ff9b8180b4e7fbb8e
Yi Lee <ylee1228@umd.edu>
2025-06-07 02:28:17 +0000

    Finish "tree compare solver" first draft

--- Helios/src/TreeCompareSolver.scala
+++ Helios/src/TreeCompareSolver.scala
@@ -3,99 +3,110 @@ import spinal.lib._
 
 class MinValWithIndex(
   width: Int = 8,
-  num_channels: Int = 8
+  num_channels: Int = 1
 ) extends Component {  
   // IO
+  val val0 = in UInt(width bits)
   val val1 = in UInt(width bits)
-  val val2 = in UInt(width bits)
+  val valid0 = in Bits(num_channels bits)
   val valid1 = in Bits(num_channels bits)
-  val valid2 = in Bits(num_channels bits)
   val valid = out Bits(2 * num_channels bits)
   val min_val = out UInt(width bits)
   // logic
+  val anyValid0 = valid0.orR
   val anyValid1 = valid1.orR
-  val anyValid2 = valid2.orR
-  val valid1_out = valid(num_channels - 1 downto 0)
-  val valid2_out = valid(2 * num_channels - 1 downto num_channels)
-  val invalid_out = B(0, num_channels bits)
-  when(anyValid1 && anyValid2) {
-    when(val1 < val2) {
-      min_val := val1
-      valid1_out := valid1
-      valid2_out := invalid_out
+  val valid0_out = valid(num_channels - 1 downto 0)
+  val valid1_out = valid(2 * num_channels - 1 downto num_channels)
+  when(anyValid0 && anyValid1) {
+    when(val0 < val1) {
+      min_val := val0
+      valid0_out := valid0
+      valid1_out := B(0)
     } otherwise {
-      min_val := val2
-      valid1_out := invalid_out
-      valid2_out := valid2
+      min_val := val1
+      valid0_out := B(0)
+      valid1_out := valid1
     }
   } otherwise {
-    when(anyValid1) {
-      min_val := val1
-      valid1_out := valid1
-      valid2_out := invalid_out
+    when(anyValid0) {
+      min_val := val0
+      valid0_out := valid0
+      valid1_out := B(0)
     } otherwise {
-      min_val := val2
-      valid1_out := invalid_out
-      valid2_out := valid2
+      min_val := val1
+      valid0_out := B(0)
+      valid1_out := valid1
     }
   }
 }
 
-class MinVal4xWithIndex(
-  width: Int = 8,
-  num_channels: Int = 8
-) extends Component {
+class MinVal4xWithIndex(width: Int = 8) extends Component {
   // IO
-  val vals = in UInt(4 * width bits)
-  val valid = in Bits(4 * num_channels bits)
+  val vals = in port Vec.fill(4)(Flow(UInt(width bits)))
   val min_val = out UInt(width bits)
-  val min_valid = out Bits(4 * num_channels bits)
+  val min_valid = out Bits(4 bits)
   // logic
-  val left = new MinValWithIndex(width, num_channels)
-  left.val1 := vals(width - 1 downto 0)
-  left.val2 := vals(2 * width - 1 downto width)
-  left.valid1 := valid(num_channels - 1 downto 0)
-  left.valid2 := valid(2 * num_channels - 1 downto num_channels)
-  val right = new MinValWithIndex(width, num_channels)
-  right.val1 := vals(3 * width - 1 downto 2 * width)
-  right.val2 := vals(4 * width - 1 downto 3 * width)
-  right.valid1 := valid(3 * num_channels - 1 downto 2 * num_channels)
-  right.valid2 := valid(4 * num_channels - 1 downto 3 * num_channels)
-  val minAll = new MinValWithIndex(width, 2 * num_channels)
-  minAll.val1 := left.min_val
-  minAll.val2 := right.min_val
-  minAll.valid1 := left.valid
-  minAll.valid2 := right.valid
+  val left = new MinValWithIndex(width)
+  left.val0 := vals(0).payload
+  left.val1 := vals(1).payload
+  left.valid0(0) := vals(0).valid
+  left.valid1(0) := vals(1).valid
+  val right = new MinValWithIndex(width)
+  right.val0 := vals(2).payload
+  right.val1 := vals(3).payload
+  right.valid0(0) := vals(2).valid
+  right.valid1(0) := vals(3).valid
+  val minAll = new MinValWithIndex(width, 2)
+  minAll.val0 := left.min_val
+  minAll.val1 := right.min_val
+  minAll.valid0 := left.valid
+  minAll.valid1 := right.valid
   min_val := minAll.min_val
   min_valid := minAll.valid
 }
 
-class MinVal8xWithIndex(
-  width: Int = 8,
-  num_channels: Int = 8
-) extends Component {
+class MinVal8xWithIndex(width: Int = 8) extends Component {
   // IO
-  val vals = in UInt(8 * width bits)
-  val valid = in Bits(8 * num_channels bits)
+  val vals = in port Vec.fill(8)(Flow(UInt(width bits)))
   val min_val = out UInt(width bits)
-  val min_valid = out Bits(8 * num_channels bits)
-  // logic
-  val left = new MinVal4xWithIndex(width, num_channels)
-  left.vals := vals(4 * width - 1 downto 0)
-  left.valid := valid(4 * num_channels - 1 downto 0)
-  val right = new MinVal4xWithIndex(width, num_channels)
-  right.vals := vals(8 * width - 1 downto 4 * width)
-  right.valid := valid(8 * num_channels - 1 downto 4 * num_channels)
-  val minAll = new MinValWithIndex(width, 4 * num_channels)
-  minAll.val1 := left.min_val
-  minAll.val2 := right.min_val
-  minAll.valid1 := left.valid
-  minAll.valid2 := right.valid
+  val min_valid = out Bits(8 bits)
+  /* -- logic -- */
+  val left = new MinVal4xWithIndex(width)
+  val right = new MinVal4xWithIndex(width)
+  // can't slice vectors...?
+  for(i <- 0 until 4) {
+    left.vals(i) := vals(i)
+    right.vals(i) := vals(i + 4)
+  }
+  val minAll = new MinValWithIndex(width, 4)
+  minAll.val0 := left.min_val
+  minAll.val1 := right.min_val
+  minAll.valid0 := left.min_valid
+  minAll.valid1 := right.min_valid
   min_val := minAll.min_val
   min_valid := minAll.valid
 }
 
-object VerilogTest extends App {
-  SpinalVerilog { new MinVal8xWithIndex() }
+class MinValLess8xWithIndex(
+  width: Int = 8,
+  num_channels: Int = 6
+) extends Component {
+  assert(num_channels <= 8)
+  /* IO */
+  val values = in port Vec.fill(num_channels)(Flow(UInt(width bits)))
+  val result = out UInt(width bits)
+  val output_valids = out Bits(num_channels bits)
+  /* logic */
+  val comp = new MinVal8xWithIndex()
+  for(i <- 0 until 8) {
+    if(i < num_channels) {
+      comp.vals(i) := values(i)
+    } else {
+      comp.vals(i).setIdle()
+    }
+  }
+  result := comp.min_val
+  for(i <- 0 until num_channels) {
+    output_valids(i) := comp.min_valid(i)
+  }
 }
-
