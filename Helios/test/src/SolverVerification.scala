49c51f363333ac297fc46f0dfa84942711c12251
Yi Lee <ylee1228@umd.edu>
2025-06-28 13:09:42 -0400

    Experiment with `Vec.asBits`

--- Helios/test/src/SolverVerification.scala
+++ Helios/test/src/SolverVerification.scala
@@ -25,20 +25,6 @@ class BoxedSolver extends BlackBox {
   setBlackBoxName("min_val_less_8x_with_index")
 }
 
-class WrappedSolver extends Component {
-  val values = in port Vec.fill(channel_count)(UInt(data_width bits))
-  val valids = in port Bits(channel_count bits)
-  val result = out port UInt(data_width bits)
-  val output_valids = out port Bits(channel_count bits)
-  val solver = new BoxedSolver()
-  for(i <- 0 until channel_count) {
-    solver.io.values(((i + 1) * data_width - 1) downto (i * data_width)) := values(i).asBits
-  }
-  solver.io.valids := valids
-  result := solver.io.result.asUInt
-  output_valids := solver.io.output_valids
-}
-
 class SolverChecker extends Component {
   val values = in port Vec.fill(channel_count)(UInt(data_width bits))
   val valids = in port Bits(channel_count bits)
@@ -48,21 +34,18 @@ class SolverChecker extends Component {
     solver.values(i).valid := valids(i)
     solver.values(i).payload := values(i)
   }
-  val reference = new WrappedSolver()
-  reference.values := values
-  reference.valids := valids
-  val valids_eqs = Vec.tabulate(channel_count) { i =>
-    solver.valids(i) === reference.output_valids(i)
-  }
-  val valids_all_eq = valids_eqs.asBits.andR
-  val res_eq = solver.result === reference.result
-  outputs_equal := valids_all_eq & ((!reference.output_valids.orR) || res_eq)
+  val reference = new BoxedSolver()
+  reference.io.values := values.asBits
+  reference.io.valids := valids
+  val valids_all_eq = solver.valids === reference.io.output_valids
+  val res_eq = solver.result === reference.io.result.asUInt
+  outputs_equal := valids_all_eq & ((!solver.valids.orR) || res_eq)
 }
 
 object SolverTest extends TestSuite {
   def tests = Tests {
     test("Checking tree solver using SymbiYosys") {
-      FormalConfig.withBMC(2).doVerify(new Component {
+      FormalConfig.withBMC(15).doVerify(new Component {
         val dut = FormalDut(new SolverChecker())
         anyconst(dut.values)
         anyconst(dut.valids)
