157ec9e587e8a0e5c2d6e51a5db61e162850deba
Yi Lee <ylee1228@umd.edu>
2025-06-30 00:56:17 -0400

    Fix code formatting

--- Helios/src/ProcessingUnit.scala
+++ Helios/src/ProcessingUnit.scala
@@ -171,12 +171,12 @@ class ProcessingUnit(address: Int) extends Component {
 
   // Calculate `neighbor_is_error`
   val neighbor_is_error_internal = Bits(neighbor_count bits)
-  neighbor_is_error_internal := (
-    stage === Stage.peeling && !child_incomplete) ?
+  neighbor_is_error_internal :=
+    (stage === Stage.peeling && !child_incomplete) ?
     (neighbor_parent_vector & child_peeling_m) | B(0)
   val neighbor_is_error_border = Bits(neighbor_count bits)
-  neighbor_is_error_border := (
-    stage === Stage.peeling && !child_incomplete && odd) ?
+  neighbor_is_error_border :=
+    (stage === Stage.peeling && !child_incomplete && odd) ?
     OHMasking.last(neighbor_is_boundary) | B(0)
   neighbor_is_error := neighbor_is_error_internal | neighbor_is_error_border
 
